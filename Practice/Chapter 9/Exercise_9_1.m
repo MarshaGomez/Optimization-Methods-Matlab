f0bd333de37fac440d2bb58c5451edac1ce60e1e
Marsha Gomez Gomez <m.gomezgomez@studenti.unipi.it>
2023-05-18 09:52:33 +0200

    Material Organize

--- Practice/Chapter 9/Exercise_9_1.m
+++ Practice/Chapter 9/Exercise_9_1.m
@@ -105,3 +105,72 @@ q = [zeros(p,1)
 
 
 [~, minimum_weak] = linprog(c,P,q, [],[],[],[],options)
+
+%% 
+% *Alternative and Manual verification*
+% 
+% Check the point $x^{\star } =\left(5,0\ldotp 5\right)$
+% 
+% Calculate $f\left(x^{\star } \right)$
+
+x1 = 5;
+x2 = 0;
+x3 = 5;
+
+f1 = x1+2.*x2-3.*x3;
+f2 = -x1-x2-x3;
+f3 = -4.*x1-2.*x2+x3;
+
+f_xopt = [f1 f2 f3]
+%% 
+% The corresponding auxiliary problem with $f\left(x^{\star } \right)=\left(-10,-10,-15\right)$
+% 
+% $$\left\lbrace \begin{array}{lll}-\textrm{minimize} & -v & \\s\ldotp t & x_1 
+% +2x_2 -3x_3 +\varepsilon_1  & \le -10\\ & -x_1 -x_2 -x_3 +\varepsilon_2  & \le 
+% -10\\ & -4x_1 -2x_2 +x_3 +\varepsilon_3  & \le -15\\ & x_1 +x_2 +x_3  & \le 
+% 10\\ & x_3  & \le 5\\ & x_1 \ldotp x_2 ,x_3  & \ge 0\\ & \varepsilon_1 ,\varepsilon_2 
+% ,\varepsilon_3  & \ge 0\\ & -v & \le \varepsilon_i \end{array}\right.$$
+
+A = [ 1  2 -3  1  0  0  0
+     -1 -1 -1  0  1  0  0
+     -4 -2  1  0  0  1  0
+      1  1  1  0  0  0  0
+      0  0  1  0  0  0  0
+      0  0  0 -1  0  0  0
+      0  0  0  0 -1  0  0
+      0  0  0  0  0 -1  0
+     -1  0  0  0  0  0  1
+      0 -1  0  0  0  0  1
+      0  0 -1  0  0  0  1];
+
+b = [-10
+     -10
+     -15
+      10 
+       5
+       0 
+       0
+       0
+       0
+       0
+       0];
+
+f = [0
+     0
+     0
+     0
+     0
+     0
+    -1];
+
+lb = [0
+      0
+      0
+      0
+      0
+      0
+     -inf];
+
+[x, fval] = linprog(f,A,b, [], [], lb);
+x
+fval
