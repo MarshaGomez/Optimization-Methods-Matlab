964f31c7de5698104812c132518f99b27c6cfe31
Marsha Gomez Gomez <m.gomezgomez@studenti.unipi.it>
2023-05-23 11:57:44 +0200

    Exam 1 Question 3

--- Exams/Exam_1.m
+++ Exams/Exam_1.m
@@ -1,7 +1,7 @@
 %% 
 % Consider the following optimization problem:
 % 
-% $$\left\lbrace \begin{array}{ll}\mathrm{minimize} & -x_1^2 +2x_1 +x_2^2 -2x_2 
+% $$\left\lbrace \begin{array}{ll}\textrm{minimize} & -x_1^2 +2x_1 +x_2^2 -2x_2 
 % \\s\ldotp t & x_1^2 -4\le 0\\\; & x_2^2 -4\le 0\end{array}\right.$$
 
 close all;
@@ -10,11 +10,27 @@ clc;
 
 matlab.lang.OnOffSwitchState = 1;
 %% 
-% a) Do global optimal solutions exist? Why?
-
-
+% *a) Do global optimal solutions exist? Why?*
+% 
+% On this problem, we have an objective function with Q indefinite and for the 
+% existence of the global optimal we can apply the Eaves theorem. 
+% 
+% $$\left\lbrace \begin{array}{ll}\textrm{minimize} & \frac{1}{2}x^T Q\;x+q^T 
+% x\\s\ldotp t & A\;x\le b\end{array}\right.$$
+% 
+% The recession cone $\Omega$ is $\textrm{rec}\left(\Omega \right)=\left\lbrace 
+% d:A\;d\le 0\right\rbrace$
+% 
+% There exists a global optimum for (P) if and only if:
 %% 
-% b) Is it a convex problem? Why?
+% * $d^T Q\;d\ge 0\;;\forall d\;\epsilon \;\textrm{rec}\left(\Omega \right)$
+% * $d^T \left(Q\;x+q\right)\ge 0\;;\forall x\;\epsilon \;\Omega \;\textrm{and}\;\forall 
+% d\;\epsilon \;\textrm{rec}\left(\Omega \right)$ such that $d^T Q\;d=0$
+%% 
+% The problem is bounded, so the Theorem is satisfied and the answer is yes, 
+% the global optimal solution exist.
+% 
+% *b) Is it a convex problem? Why?*
 % 
 % It is a quadratic optimization problem and to prove that it is a convex optimization 
 % problem we need to show that both the objective function and the constraints 
@@ -23,13 +39,13 @@ matlab.lang.OnOffSwitchState = 1;
 % * Objective Function: $f\left(x\right)=-x_1^2 +2x_1 +x_2^2 -2x_2$  
 %% 
 % The objective function is quadratic convex minimization of the form: $f\left(x\right)=\frac{1}{2}x^T 
-% \textrm{Qx}+q^T x+b$ . To prove convexity, we need to show that the Hessian 
-% matrix Q is positive semidefinite. A matrix Q is positive semidefinite if the 
-% eigenvalues of Q are non-negative. 
+% Q\;x+q^T x+b$ . To prove convexity, we need to show that the Hessian matrix 
+% Q is positive semidefinite. A matrix Q is positive semidefinite if the eigenvalues 
+% of Q are non-negative. 
 %% 
 % * Constraints: 
-% * $g_1 \left(x\right)=$$x_1^2 -4\le 0$ 
-% * $g_2 \left(x\right)=$$x_1^2 -4\le 0$
+% * $g_1 \left(x\right)=x_1^2 -4\le 0$ 
+% * $g_2 \left(x\right)=x_1^2 -4\le 0$
 %% 
 % The constraints in a quadratic convex minimization is presented on the form: 
 % $g_i \left(x\right)=\frac{1}{2}x^T A_i x+{a_i }^T x+c_i \le 0\;;\;\forall i=1,\ldotp 
@@ -122,3 +138,45 @@ ylim([-4 4]);
 
 % Display the plot
 hold off
+%% 
+% *c) Does the Abadie constraints qualification hold in any feasible point? 
+% Why?*
+% 
+% We need to check the Abadie Second-Order condition to ensure the existence 
+% of a Karuch-Kuhn-Tucker point (which is a point that satisfies the KKT conditions).
+% 
+% Theorem - Sufficient conditions for ACQ
+%% 
+% * (Linear independence of the gradient of active constraints): If $\overline{x} 
+% \;\epsilon \;\Omega$ and the vectors are independent, then ACQ holds at $\overline{x}$:
+%% 
+% $$\left\lbrace \begin{array}{ll}\nabla g_i \left(\overline{x} \right) & \mathrm{for}\;i\;\epsilon 
+% \;A\left(\overline{x} \right)\\\nabla h_j \left(\overline{x} \right) & \mathrm{for}\;j=1,\ldotp 
+% \ldotp \ldotp ,p\end{array}\right.$$
+% 
+% 
+% 
+% We need to solve the optimization problem and examine the KKT conditions: 
+%% 
+% # Direction wich is feasible, $\nabla f\left(x\right)+\sum_{i=1}^m \lambda_i 
+% \nabla g_i \left(x\right)+\sum_{j=1}^p \mu_j \nabla h_j \left(x\right)$
+% # Feasible, $g\left(x\right)\le 0\;;h\left(x\right)=0$
+% # Complement slackness, $\lambda_i \left(g_i \left(x\right)\right)=0$
+% # Lagrange non-negativity $\lambda \ge 0$
+%% 
+% On this exercise the conditions are as follows:
+% 
+% 
+% 
+% $$\left\lbrace \begin{array}{ll}-2x_1 +2+2\lambda_1 x_1  & =0\\2x_2 -2+2\lambda_2 
+% x_2  & =0\\x_1^2 -4 & \le 0\\x_2^2 -4 & \le 0\\\lambda_1 \left(x_1^2 -4\right) 
+% & =0\\\lambda_2 \left(x_2^2 -4\right) & =0\\\lambda_1  & \ge 0\\\lambda_2  & 
+% \ge 0\end{array}\right.$$
+% 
+% From the complementary slackness conditions, we can observe that is a constraints 
+% is inactive(not-binding), the corresponding Lagrange multiplier is zero. Therefore, 
+% is $\lambda_1 >0$ that $x_1^2 -4\le 0$ is an active constraint, and if $\lambda_2 
+% >0$ that $x_2^2 -4\le 0$  is an active contraint.
+% 
+% With this observations, we can conclude that the Abadie Constraint Qualification 
+% holds at any feasible point for the given optimization problem.
