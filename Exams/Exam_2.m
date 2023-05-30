a351b9bc5ca0abb7135cf2fa1f6ce47d821cc116
Marsha Gomez Gomez <m.gomezgomez@studenti.unipi.it>
2023-05-30 11:43:51 +0200

    Exam 2

--- Exams/Exam_2.m
+++ Exams/Exam_2.m
@@ -12,12 +12,32 @@ clc;
 matlab.lang.OnOffSwitchState = 1;
 %% 
 % *a) Do global optimal solutions exist? Why?*
+% 
+% To determine convexity, we need to examine the Hessian Matrix, the second 
+% partial derivates of the objective function with respect to the variables. On 
+% the next 1.2 we can check that our Hessian Matrix is strongly convex and we 
+% know that in any convex optimization problem the feasible region is a convex 
+% set. 
+% 
+% Using the Theorem weierstrass that says if the objective funciton is continuous 
+% and the feasible region is closed and bounded, then at least a global optimum 
+% exists. 
+% 
+% On this problem using Collory 3 were the objective function is continuous 
+% and coercive and the feasible region is closed, then at least a global minimum 
+% exists. Coercivity is$\lim_{\left\|x\right\|\to \infty } f\left(x\right)=+\infty$
 
+% In this case we use 1 variable, because is an approximation calculation
+objective_function = @(x) 2*x^2 + x*x + x^2 + 2*x*x + 3*x^2 + x*x + 2*x^2 - 5*x - 4*x + 3*x;
+syms x
 
+limit = limit(objective_function(x), x, Inf)
 %% 
+% Since the objective function is coercive, there exists a global optimum.
+% 
 % *b) Is it a convex problem? Why?*
 % 
-% We need to analyze the covexity of each term and the objective function as 
+% We need to analyze the convexity of each term and the objective function as 
 % a whole. A function is convex if the Hessian Matrix is positive semi-definite. 
 % 
 % In this case the objective function is:
@@ -45,10 +65,11 @@ Q = [4 0 1 0
 
 fprintf('Eigen Values Objective Function:');
 display(eig(Q))
+fprintf('Determinant: %i', det(Q));
 %% 
-% Since all the eigenvalues are positive and not zero, we can conclude that 
-% Hessian Matrix is positive definite, therefore is a strongly convex optimization 
-% problem.
+% Since all the eigenvalues are positive and not zero, also the Determinant 
+% of the Hessian matrix positive definite, we can conclude that Hessian Matrix 
+% is positive definite, therefore is a strongly convex optimization problem.
 % 
 % *c) Find the global minimum by using the gradient method with exact line search.*
 % 
@@ -110,8 +131,10 @@ while (norm(g)>tolerance)
     % Next iteration
     i = i+1;
 end
-
 %% 
+% With Gradient Method using exact line search we need 48 iterations to arrive 
+% to the best solution.
+% 
 % *d) Find the global minimum by using the conjugate gradient method* 
 % 
 % Starting from the point (0, 0, 0, 0). How many iterations are needed? Write 
@@ -119,15 +142,15 @@ end
 % direction involves the gradient computed at the current iteration and the direction 
 % computed at the precious iteration. 
 % 
-% $$d^k =\left\lbrace \begin{array}{ll}-g^0  & \mathrm{if}\;k=0\\-g^k +\beta_k 
-% d^{k-1}  & \mathrm{if}\;k\ge 1\end{array}\right.$$
+% $$d^k =\left\lbrace \begin{array}{ll}-g^0  & \textrm{if}\;k=0\\-g^k +\beta_k 
+% d^{k-1}  & \textrm{if}\;k\ge 1\end{array}\right.$$
 % 
 % The optimization problem is unconstraint and the objective function is a quadratic, 
 % where Q is positive definite, as follows:
 % 
 % $$\begin{array}{l}f\left(x\right)=\frac{1}{2}x^T Q\;x+q^T x\\g\left(x\right)=Q\;x+q;\end{array}$$
 % 
-% Conjugate gradient method for quadratic functions:
+% Conjugate gradient method for quadratic functions with linear search:
 %%
 % 
 %   % Conjugate method
@@ -163,6 +186,7 @@ fprintf("\n \n Conjugate Gradient with exact line search \n");
 fprintf("i \t f(x) \t \t \t ||grad f(x)|| ");
 
 while (norm(g)>tolerance)
+    fprintf('%i \t %2.10f \t \t %2.10f \n', i, v, norm(g));
     if i == 0
         % Direction on the first iteration
         d = -g;
@@ -175,8 +199,7 @@ while (norm(g)>tolerance)
     % Step Size
     alpha = (norm(g)^2) / (d'*Q*d);
     x = x + alpha*d;
-    
-    fprintf('%i \t %2.10f \f %2.10f \n', i, v, norm(g));
+   
     d_prev = d;
     g_prev = g;
 
@@ -186,6 +209,54 @@ while (norm(g)>tolerance)
     i = i + 1;
 end
 %% 
+% On this case, we can easily see that the convergence rate is quicklier than 
+% the previous method, with Conjugate Method using exact line search we use just 
+% 3 iterations to found the minimum.
+% 
 % *e) Find the global minimum by using the Newton method* 
 % 
 % Starting from the point (0, 0, 0, 0). How many iterations are needed?
+% 
+% We can conclude an objective function is strongly convex because the Hessian 
+% of the function is positive definite, and we get the global convergence because 
+% the direction is a descent direction:
+% 
+% $$\nabla f{\left(x^k \right)}^T d^k =-\nabla f{\left(x^k \right)}^T {\left\lbrack 
+% \nabla^2 f\left(x^k \right)\right\rbrack }^{-1} \nabla \left(f\left(x^k \right)\right)<0$$
+% 
+% Newton method basic version:
+%%
+% 
+%   % Newton method
+%   Choose x0 i=0
+%   while (g ~= 0)
+%       [search direction] d = -H\g;
+%       x = x + d 
+%       i = i + 1;
+%   end
+%
+%% 
+% 
+
+x0 = [0
+      0
+      0
+      0];
+
+i = 0;
+x = x0;
+
+v = (1/2)*x'*Q*x + q'*x;
+g = Q*x + q;
+
+fprintf("\n \n Newton method basic version \n");
+fprintf("i \t f(x) \t \t \t ||grad f(x)|| \n");
+while (norm(g) > tolerance)
+    fprintf("%i \t %2.10f \t \t %2.10f \n", i, v, norm(g));
+    d = -Q\g;
+    x = x + d;
+    i = i + 1;
+
+    v = (1/2)*x'*Q*x + q'*x;
+    g = Q*x + q;
+end
